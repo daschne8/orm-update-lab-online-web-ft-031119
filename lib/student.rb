071f5b602a082851679fff422f405b70ca65984c
Daniel Schneider <daschne8@gmail.com>
2019-04-10 17:56:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,19 @@ class Student
 attr_accessor :name,:grade
 attr_reader :id
 
-def initialize(name,grade,id = nil)
-  @name,@grade,@id = name,grade,id
-end
+  def initialize(name,grade,id = nil)
+    @name,@grade,@id = name,grade,id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT)
+      SQL
+    DB[:conn].execute(sql)
+  end
 
 
 end
